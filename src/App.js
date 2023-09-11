a9c6fff343abf7dc07d584b091b8d0a0850fe336
marinahaugen <marina.santos.haugen@dnb.no>
2023-09-11 19:08:10 +0200

    Cleaning up and trying to call the grafana endpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://grafana.tech.dnb.no/api/dashboards/tags', {
+          headers: {
+            Authorization: `Bearer ${process.env.GRAFANA_API_KEY}`,
+          },
+        });
+        const data = await response.json();
+        console.log(data);
+        setData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React. Test from Marina
-        </a>
-      </header>
+      <header className="App-header" />
+      <div>{data ? JSON.stringify(data) : 'Loading...'}</div>
     </div>
   );
 }
