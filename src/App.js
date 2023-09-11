0d95896b96c6e6a32e0d476d1db3ac34f563e175
marinahaugen <marina.santos.haugen@dnb.no>
2023-09-11 17:49:51 +0200

    Creating a standard react app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React. Test from Marina
         </a>
       </header>
     </div>
